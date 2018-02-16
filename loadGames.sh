31f4b441ce984ad68853a11facae134d11fa7733
Jens Giehl <mobil.jgiehl@gmail.com>
2018-02-16 20:20:23 +0100

    Added wgetRetry as submodule

--- loadGames.sh
+++ loadGames.sh
@@ -14,4 +14,4 @@ fi
 currentDate=`date '+%Y-%m-%d_%H%M'` 
 fileName=${1:-"$STEAM_ID-GAMES-$currentDate"}
 #wget -q "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_KEY&steamid=$STEAM_ID&format=json&include_appinfo=1&include_played_free_games=1" -O $fileName.json
-./wgetRetry.sh "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_KEY&steamid=$STEAM_ID&format=json&include_appinfo=1&include_played_free_games=1" $fileName.json
+source wgetRetry/wgetRetry.sh "http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=$STEAM_KEY&steamid=$STEAM_ID&format=json&include_appinfo=1&include_played_free_games=1" $fileName.json
